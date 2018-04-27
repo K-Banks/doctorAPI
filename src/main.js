bd2cf9df2ae70f235b5900f39b0b2ab9c38a5441
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-27 15:37:09 -0700

    Add functionality to let user input location within the United States

--- src/main.js
+++ src/main.js
@@ -7,24 +7,34 @@ import { API } from './api.js';
 import { Doctor } from './doctorInfo.js';
 import { Output } from './output.js';
 
+// Hide Output field upon loading page. Avoid conflict w/styling
+$("div.output").hide();
+
 $(document).ready(function(){
   $('form#mdIssueForm').submit(function(event) {
     event.preventDefault();
-    let apiCall = new API();
-    let outputCall = new Output();
+    const apiCall = new API();
+    const outputCall = new Output();
     let issueSearchString = $("#issueStringSubmission").val();
     $("#issueStringSubmission").val("");
     $("div.output").hide();
+    $("div.output").empty();
     $("div.noResult").hide();
     $("div.errorOuput").hide();
     $("span#searchString").text(issueSearchString);
+    $("p#searchStore").show();
 
-    let promiseSearch = apiCall.SearchByIssue(issueSearchString);
+    let userCity = $("input#userCity").val();
+    let userState = $("input#userState").val();
+    let userLocation = userState + "-" + userCity;
+    userLocation = userLocation.toLowerCase();
+
+    let promiseSearch = apiCall.SearchByIssue(issueSearchString, userLocation);
     promiseSearch.then(function(response) {
       if (response.data[0] != undefined) {
         let doctors = response.data;
         doctors.forEach(function(doctor) {
-          let practice = response.data[0].practices[0];
+          let practice = doctor.practices[0];
           let thisDoctor = new Doctor(doctor);
           let practiceInformation = thisDoctor.PracticeParser(practice);
           outputCall.DoctorList(thisDoctor, practiceInformation);
