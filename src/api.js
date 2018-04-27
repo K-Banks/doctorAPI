11084bbc6b4c723240a6291d65c57d9998ea306f
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-27 13:44:36 -0700

    Modify search function to take an input from user

--- src/api.js
+++ src/api.js
@@ -19,6 +19,24 @@ class API {
     return promise;
   }
 
+  SearchByIssue(searchString) {
+    let promise = new Promise(function(resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=3&user_key=${apiKey}&query=${searchString}`);
+      xhr.send();
+      xhr.onload = function() {
+        if (this.status === 200) {
+          let response = JSON.parse(xhr.response);
+          resolve(response);
+        } else {
+          reject(Error(xhr.statusText));
+        }
+      }
+    });
+    return promise;
+  }
+
+
 }
 
 export { API }
--- src/main.js
+++ src/main.js
@@ -8,12 +8,15 @@ import { Doctor } from './doctorInfo.js';
 import { Output } from './output.js';
 
 $(document).ready(function(){
-  $('form#userForm').submit(function(event) {
+  $('form#mdIssueForm').submit(function(event) {
     event.preventDefault();
     let apiCall = new API();
     let outputCall = new Output();
-    let promiseSearch = apiCall.testSearch();
+    let issueSearchString = $("#issueStringSubmission").val();
+    $("#issueStringSubmission").val("");
+    $("span#searchString").text(issueSearchString);
 
+    let promiseSearch = apiCall.SearchByIssue(issueSearchString);
     promiseSearch.then(function(response) {
       let doctors = response.data;
       doctors.forEach(function(doctor) {
@@ -22,64 +25,6 @@ $(document).ready(function(){
         let practiceInformation = thisDoctor.PracticeParser(practice);
         outputCall.DoctorList(thisDoctor, practiceInformation);
       });
-
-      // refactor to dynamically add multiple doctors' information
-      // if (doctor.first_name != undefined && doctor.last_name != undefined && doctor.title != undefined) {
-      //   $("div.output").append(`
-      //     <div class="card thumbnail">
-      //       <img class="card-img-top" src="${doctor.image}" alt="Image of Dr. ${doctor.first_name} ${doctor.last_name}">
-      //       <h4 class = "card-title name">${doctor.first_name} ${doctor.last_name}, ${doctor.title}</h4>
-      //       <div id="${doctor.last_name}">
-      //         <h4>Practice information:</h4>
-      //       </div>
-      //   `);
-      //   let practiceTarget = `div#${doctor.last_name}`;
-      //   if (practiceInformation[1] != undefined && practiceInformation[2] != undefined && practiceInformation[3] != undefined && practiceInformation[4] != undefined) {
-      //     $(practiceTarget).append(`
-      //       <p>Name: ${practiceInformation[0]}</p>
-      //       <p>Address: ${practiceInformation[1]}, ${practiceInformation[2]}, ${practiceInformation[3]} ${practiceInformation[4]}</p>
-      //     `);
-      //   } else {
-      //     $(practiceTarget).append(`
-      //       <p>Name: ${practiceInformation[0]}</p>
-      //       <p>Address: A valid address could not be found for this practice.</p>
-      //     `);
-      //   }
-      //   if (practiceInformation[5] != undefined) {
-      //     $(practiceTarget).append(`
-      //       <p>Phone: ${practiceInformation[5]}</p>
-      //     `);
-      //   } else {
-      //     $(practiceTarget).append(`
-      //       <p>Phone: A valid phone number could not be found for this practice.</p>
-      //     `);
-      //   }
-      //   if (practiceInformation[6] != undefined) {
-      //     $(practiceTarget).append(`
-      //       <p>Website: ${practiceInformation[6]}</p>
-      //     `);
-      //   } else {
-      //     $(practiceTarget).append(`
-      //       <p>Website: A valid website could not be found for this practice.</p>
-      //     `);
-      //   }
-      //   if (practiceInformation[7] === true) {
-      //     $(practiceTarget).append(`
-      //       <p>Is this practice accepting new patients? <strong>Yes</strong></p>
-      //     `);
-      //   } else if (practiceInformation[7] === false) {
-      //     $(practiceTarget).append(`
-      //       <p>Is this practice accepting new patients? <strong>No</strong></p>
-      //     `);
-      //   } else {
-      //     $(practiceTarget).append(`
-      //       <p>No information is available on whether this practice is accepting new patients or not.</p>
-      //     `);
-      //   }
-      // } else {
-      //   console.log("This doctor has invalid information and should not be displayed to user.");
-      // }
-
     }, function(error) {
       console.log(error);
     });
