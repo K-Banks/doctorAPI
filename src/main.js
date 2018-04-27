5e2295e84f43e3a0b529f19dcb4a1411012080ad
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-27 11:30:23 -0700

    Refactor format for doctor information, add logic to exclude details with no information

--- src/main.js
+++ src/main.js
@@ -19,17 +19,34 @@ $(document).ready(function(){
       let practiceInformation = doctor.PracticeParser(practice);
 
       // add data to front end using jquery
-      $("span#firstName").text(doctor.first_name);
-      $("span#lastName").text(doctor.last_name);
-      $("span#title").text(doctor.title);
-      $("span#practiceName").text(practiceInformation[0]);
-      $("span#practiceStreet").text(practiceInformation[1]);
-      $("span#practiceCity").text(practiceInformation[2]);
-      $("span#practiceState").text(practiceInformation[3]);
-      $("span#practiceZip").text(practiceInformation[4]);
-      $("span#practicePhone").text(practiceInformation[5]);
-      $("span#practiceWebsite").text(practiceInformation[6]);
-      $("span#practicePatient").text(practiceInformation[7]);
+      if (doctor.first_name != undefined && doctor.last_name != undefined && doctor.title != undefined) {
+        $("span#doctorName").text(`${doctor.first_name} ${doctor.last_name}, ${doctor.title}`);
+        $("span#practiceName").text(practiceInformation[0]);
+        if (practiceInformation[1] != undefined && practiceInformation[2] != undefined && practiceInformation[3] != undefined && practiceInformation[4] != undefined) {
+          $("span#practiceAddress").text(`${practiceInformation[1]}, ${practiceInformation[2]}, ${practiceInformation[3]} ${practiceInformation[4]}`);
+        } else {
+          $("span#practiceAddress").text('A valid address could not be found for this practice.');
+        }
+        if (practiceInformation[5] != undefined) {
+          $("span#practicePhone").text(practiceInformation[5]);
+        } else {
+          $("span#practicePhone").text('A valid phone number could not be found for this practice.')
+        }
+        if (practiceInformation[6] != undefined) {
+          $("span#practiceWebsite").text(practiceInformation[6]);
+        } else {
+          $("span#practiceWebsite").text('A valid website could not be found for this practice.')
+        }
+        if (practiceInformation[7] === true) {
+          $("span#practicePatient").text("This practice is currently accepting new patients.");
+        } else if (practiceInformation[7] === false) {
+          $("span#practicePatient").text("This practice is not currently accepting new patients.")
+        } else {
+          $("span#practicePatient").text('No information is available on whether this practice is accepting new patients or not.')
+        }
+      } else {
+        console.log("This doctor has invalid information and should not be displayed to user.");
+      }
 
       // add image to profile
       let responseImage = document.getElementById("image");
