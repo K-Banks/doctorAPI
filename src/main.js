254048b9c4f83bc996e0d368d7da37b172186795
Kayl Eubanks <kayleubanks@gmail.com>
2018-04-27 14:35:07 -0700

    Add logic for handling errors, response with no results. Styling for better UX

--- src/main.js
+++ src/main.js
@@ -14,18 +14,28 @@ $(document).ready(function(){
     let outputCall = new Output();
     let issueSearchString = $("#issueStringSubmission").val();
     $("#issueStringSubmission").val("");
+    $("div.output").hide();
+    $("div.noResult").hide();
+    $("div.errorOuput").hide();
     $("span#searchString").text(issueSearchString);
 
     let promiseSearch = apiCall.SearchByIssue(issueSearchString);
     promiseSearch.then(function(response) {
-      let doctors = response.data;
-      doctors.forEach(function(doctor) {
-        let practice = response.data[0].practices[0];
-        let thisDoctor = new Doctor(doctor);
-        let practiceInformation = thisDoctor.PracticeParser(practice);
-        outputCall.DoctorList(thisDoctor, practiceInformation);
-      });
+      if (response.data[0] != undefined) {
+        let doctors = response.data;
+        doctors.forEach(function(doctor) {
+          let practice = response.data[0].practices[0];
+          let thisDoctor = new Doctor(doctor);
+          let practiceInformation = thisDoctor.PracticeParser(practice);
+          outputCall.DoctorList(thisDoctor, practiceInformation);
+          $("div.output").show();
+        });
+      } else {
+        $("div.noResult").show();
+      }
     }, function(error) {
+      $("h1#errorShow").text(`There was an error processing this search. Please try again. (Error message: ${error})`);
+      $("div.errorOutput").show();
       console.log(error);
     });
   });
